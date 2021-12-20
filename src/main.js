4a3ef518fd3d17c56c1788f97fc8c403a6790444
james23216 <jhmun23216@gmail.com>
2021-12-19 18:42:36 -0800

    first time to push the final product

---
+++ src/main.js
@@ -0,0 +1,511 @@
+const basePath = process.cwd();
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+const fs = require("fs");
+const sha1 = require(`${basePath}/node_modules/sha1`);
+const { createCanvas, loadImage } = require(`${basePath}/node_modules/canvas`);
+const buildDir = `${basePath}/build`;
+const layersDir = `${basePath}/layers`;
+let tools = require('simple-svg-tools');
+const {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+} = require(`${basePath}/src/config.js`);
+const canvas = createCanvas(format.width, format.height);
+const ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = format.smoothing;
+var metadataList = [];
+var attributesList = [];
+var dnaList = new Set();
+const DNA_DELIMITER = "-";
+const HashlipsGiffer = require(`${basePath}/modules/HashlipsGiffer.js`);
+
+let hashlipsGiffer = null;
+
+const buildSetup = () => {
+  if (fs.existsSync(buildDir)) {
+    fs.rmdirSync(buildDir, { recursive: true });
+  }
+  fs.mkdirSync(buildDir);
+  fs.mkdirSync(`${buildDir}/json`);
+  fs.mkdirSync(`${buildDir}/images`);
+  if (gif.export) {
+    fs.mkdirSync(`${buildDir}/gifs`);
+  }
+};
+
+const getRarityWeight = (_str) => {
+  let nameWithoutExtension = _str.slice(0, -4);
+  var nameWithoutWeight = Number(
+    nameWithoutExtension.split(rarityDelimiter).pop()
+  );
+  if (isNaN(nameWithoutWeight)) {
+    nameWithoutWeight = 1;
+  }
+  return nameWithoutWeight;
+};
+
+const cleanDna = (_str) => {
+  const withoutOptions = removeQueryStrings(_str);
+  var dna = Number(withoutOptions.split(":").shift());
+  return dna;
+};
+
+const cleanName = (_str) => {
+  let nameWithoutExtension = _str.slice(0, -4);
+  var nameWithoutWeight = nameWithoutExtension.split(rarityDelimiter).shift();
+  return nameWithoutWeight;
+};
+
+const getElements = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+    .map((i, index) => {
+      return {
+        id: index,
+        name: cleanName(i),
+        filename: i,
+        path: `${path}${i}`,
+        weight: getRarityWeight(i),
+      };
+    });
+};
+
+const layersSetup = (layersOrder) => {
+  const layers = layersOrder.map((layerObj, index) => ({
+    id: index,
+    elements: getElements(`${layersDir}/${layerObj.name}/`),
+    name:
+      layerObj.options?.["displayName"] != undefined
+        ? layerObj.options?.["displayName"]
+        : layerObj.name,
+    blend:
+      layerObj.options?.["blend"] != undefined
+        ? layerObj.options?.["blend"]
+        : "source-over",
+    opacity:
+      layerObj.options?.["opacity"] != undefined
+        ? layerObj.options?.["opacity"]
+        : 1,
+    bypassDNA:
+      layerObj.options?.["bypassDNA"] !== undefined
+        ? layerObj.options?.["bypassDNA"]
+        : false,
+  }));
+  return layers;
+};
+
+const saveImage = (_editionCount) => {
+  fs.writeFileSync(
+    `${buildDir}/images/${_editionCount}.png`,
+    canvas.toBuffer("image/png")
+  );
+};
+
+const genColor = () => {
+  let hue = Math.floor(Math.random() * 360);
+  let pastel = `hsl(${hue}, 100%, ${background.brightness})`;
+  return pastel;
+};
+
+let backgroundColor;
+
+const drawBackground = () => {
+  backgroundColor = genColor();
+  ctx.fillStyle = background.static ? background.default : backgroundColor;
+  ctx.fillRect(0, 0, format.width, format.height);
+};
+
+let bodyColors = [
+  { name: "Blue", hash: '#0B1324' }, //0
+  { name: "Red", hash: '#D4B771' }, //1
+  { name: "Black", hash: '#F86EA0' }, //2
+  { name: "Orange", hash: '#4B5320' }, //3
+  { name: "Yellow", hash: '#83D5FF' }, //4
+  { name: "Purple", hash: '#9B3D21' }, //5
+  { name: "White", hash: '#104587' }, //6
+  { name: "Maroon", hash: '#EB1C24' }, //7
+  { name: "Turquoise", hash: '#44E192' }, //8
+  { name: "One", hash: '#FF771E' }, //9
+  { name: "Two", hash: '#F9DF7B' }, //10
+  { name: "Three", hash: '#CBCBCB' }, //11
+  { name: "Four", hash: '#DE8D59' }  //12
+];
+
+const getRandomBodyColor = () => {
+  const bodyColor = bodyColors[Math.floor(Math.random() * bodyColors.length)];
+  return bodyColor;
+}
+
+let pantsColors = [
+  { name: "Blue", hash: '#0B1324' }, //0
+  { name: "Red", hash: '#D4B771' }, //1
+  { name: "Black", hash: '#F86EA0' }, //2
+  { name: "Orange", hash: '#4B5320' }, //3
+  { name: "Yellow", hash: '#83D5FF' }, //4
+  { name: "Purple", hash: '#9B3D21' }, //5
+  { name: "White", hash: '#104587' }, //6
+  { name: "Maroon", hash: '#EB1C24' }, //7
+  { name: "Turquoise", hash: '#44E192' }, //8
+  { name: "One", hash: '#FF771E' }, //9
+  { name: "Two", hash: '#F9DF7B' }, //10
+  { name: "Three", hash: '#CBCBCB' }, //11
+  { name: "Four", hash: '#DE8D59' }  //12
+];
+
+const getRandomPantsColor = () => {
+  const bodyColor = bodyColors[Math.floor(Math.random() * bodyColors.length)];
+  return bodyColor;
+}
+
+const addMetadata = (_dna, _edition) => {
+  let dateTime = Date.now();
+  attributesList.push({
+    trait_type: "Color",
+    value: elepantBodyColor.name,
+  });
+  attributesList.push({
+    trait_type: "Pants Color",
+    value: elepantPantsColor.name,
+  });
+  attributesList.push({
+    trait_type: "Background Color",
+    value: backgroundColor,
+  });
+  let tempMetadata = {
+    name: `${namePrefix} #${_edition}`,
+    description: description,
+    image: `${baseUri}/${_edition}.png`,
+    dna: sha1(_dna),
+    edition: _edition,
+    date: dateTime,
+    ...extraMetadata,
+    seller_fee_basis_points: 500,
+    attributes: attributesList,
+    compiler: "Elepants Engine 1.0",
+  };
+  if (network == NETWORK.sol) {
+    tempMetadata = {
+      //Added metadata for solana
+      name: tempMetadata.name,
+      symbol: solanaMetadata.symbol,
+      description: tempMetadata.description,
+      //Added metadata for solana
+      seller_fee_basis_points: solanaMetadata.seller_fee_basis_points,
+      image: `image.svg`,
+      //Added metadata for solana
+      external_url: solanaMetadata.external_url,
+      edition: _edition,
+      ...extraMetadata,
+      attributes: tempMetadata.attributes,
+      properties: {
+        files: [
+          {
+            uri: "image.svg",
+            type: "image/svg",
+          },
+        ],
+        category: "image",
+        creators: solanaMetadata.creators,
+      },
+    };
+  }
+  metadataList.push(tempMetadata);
+  attributesList = [];
+};
+
+const addAttributes = (_element) => {
+  let selectedElement = _element.layer.selectedElement;
+  if (_element.layer.name != "Body"
+      && _element.layer.name != "Head" 
+      &&_element.layer.name != "Hand") {
+    attributesList.push({
+      trait_type: _element.layer.name,
+      value: selectedElement.name,
+    });
+  }
+};
+
+const loadLayerImg = async (_layer) => {
+  return new Promise(async (resolve) => {
+    /*
+    let contents;
+    console.log(_layer.selectedElement.path);
+    console.log('before the try');
+    try {
+      contents = fs.readFileSync(_layer.selectedElement.path, 'utf8');
+    } catch(err){
+      console.error(err);
+    }
+    console.log('after the try');
+    // console.log(contents);
+    contents = contents.replace(/#BBBCBE/ig, elepantBodyColor);
+    contents = contents.replace(/#818385/ig, elepantBodyColor);
+    fs.writeFile('test.svg', contents, function (err) {
+      if (err) return console.log(err);
+      console.log('Hello World > helloworld.txt');
+    });
+    */
+    const image = await loadImage(`${_layer.selectedElement.path}`);
+    resolve({ layer: _layer, loadedImage: image, path: _layer.selectedElement.path });
+  });
+};
+
+const addText = (_sig, x, y, size) => {
+  ctx.fillStyle = text.color;
+  ctx.font = `${text.weight} ${size}pt ${text.family}`;
+  ctx.textBaseline = text.baseline;
+  ctx.textAlign = text.align;
+  ctx.fillText(_sig, x, y);
+};
+
+const drawElement = (_renderObject, _index, _layersLen) => {
+  ctx.globalAlpha = _renderObject.layer.opacity;
+  ctx.globalCompositeOperation = _renderObject.layer.blend;
+  text.only
+    ? addText(
+        `${_renderObject.layer.name}${text.spacer}${_renderObject.layer.selectedElement.name}`,
+        text.xGap,
+        text.yGap * (_index + 1),
+        text.size
+      )
+    : ctx.drawImage(
+        _renderObject.loadedImage,
+        0,
+        0,
+        format.width,
+        format.height
+      );
+
+  addAttributes(_renderObject);
+};
+
+const constructLayerToDna = (_dna = "", _layers = []) => {
+  let mappedDnaToLayers = _layers.map((layer, index) => {
+    let selectedElement = layer.elements.find(
+      (e) => e.id == cleanDna(_dna.split(DNA_DELIMITER)[index])
+    );
+    return {
+      name: layer.name,
+      blend: layer.blend,
+      opacity: layer.opacity,
+      selectedElement: selectedElement,
+    };
+  });
+  return mappedDnaToLayers;
+};
+
+/**
+ * In some cases a DNA string may contain optional query parameters for options
+ * such as bypassing the DNA isUnique check, this function filters out those
+ * items without modifying the stored DNA.
+ *
+ * @param {String} _dna New DNA string
+ * @returns new DNA string with any items that should be filtered, removed.
+ */
+const filterDNAOptions = (_dna) => {
+  const dnaItems = _dna.split(DNA_DELIMITER);
+  const filteredDNA = dnaItems.filter((element) => {
+    const query = /(\?.*$)/;
+    const querystring = query.exec(element);
+    if (!querystring) {
+      return true;
+    }
+    const options = querystring[1].split("&").reduce((r, setting) => {
+      const keyPairs = setting.split("=");
+      return { ...r, [keyPairs[0]]: keyPairs[1] };
+    }, []);
+
+    return options.bypassDNA;
+  });
+
+  return filteredDNA.join(DNA_DELIMITER);
+};
+
+/**
+ * Cleaning function for DNA strings. When DNA strings include an option, it
+ * is added to the filename with a ?setting=value query string. It needs to be
+ * removed to properly access the file name before Drawing.
+ *
+ * @param {String} _dna The entire newDNA string
+ * @returns Cleaned DNA string without querystring parameters.
+ */
+const removeQueryStrings = (_dna) => {
+  const query = /(\?.*$)/;
+  return _dna.replace(query, "");
+};
+
+const isDnaUnique = (_DnaList = new Set(), _dna = "") => {
+  const _filteredDNA = filterDNAOptions(_dna);
+  return !_DnaList.has(_filteredDNA);
+};
+
+const createDna = (_layers) => {
+  let randNum = [];
+  _layers.forEach((layer) => {
+    var totalWeight = 0;
+    layer.elements.forEach((element) => {
+      totalWeight += element.weight;
+    });
+    // number between 0 - totalWeight
+    let random = Math.floor(Math.random() * totalWeight);
+    for (var i = 0; i < layer.elements.length; i++) {
+      // subtract the current weight from the random weight until we reach a sub zero value.
+      random -= layer.elements[i].weight;
+      if (random < 0) {
+        return randNum.push(
+          `${layer.elements[i].id}:${layer.elements[i].filename}${
+            layer.bypassDNA ? "?bypassDNA=true" : ""
+          }`
+        );
+      }
+    }
+  });
+  return randNum.join(DNA_DELIMITER);
+};
+
+const writeMetaData = (_data) => {
+  fs.writeFileSync(`${buildDir}/json/_metadata.json`, _data);
+};
+
+const saveMetaDataSingleFile = (_editionCount) => {
+  let metadata = metadataList.find((meta) => meta.edition == _editionCount);
+  debugLogs
+    ? console.log(
+        `Writing metadata for ${_editionCount}: ${JSON.stringify(metadata)}`
+      )
+    : null;
+  fs.writeFileSync(
+    `${buildDir}/json/${_editionCount}`,
+    JSON.stringify(metadata, null, 2)
+  );
+};
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+  return array;
+}
+
+let elepantBodyColor;
+let elepantPantsColor;
+
+const startCreating = async () => {
+  let layerConfigIndex = 0;
+  let editionCount = 1;
+  let failedCount = 0;
+  let abstractedIndexes = [];
+  for (
+    let i = network == NETWORK.sol ? 0 : 1;
+    i <= layerConfigurations[layerConfigurations.length - 1].growEditionSizeTo;
+    i++
+  ) {
+    abstractedIndexes.push(i);
+  }
+  if (shuffleLayerConfigurations) {
+    abstractedIndexes = shuffle(abstractedIndexes);
+  }
+  debugLogs
+    ? console.log("Editions left to create: ", abstractedIndexes)
+    : null;
+  while (layerConfigIndex < layerConfigurations.length) {
+    const layers = layersSetup(
+      layerConfigurations[layerConfigIndex].layersOrder
+    );
+    while (
+      editionCount <= layerConfigurations[layerConfigIndex].growEditionSizeTo
+    ) {
+      let newDna = createDna(layers);
+      if (isDnaUnique(dnaList, newDna)) {
+        let results = constructLayerToDna(newDna, layers);
+        let loadedElements = [];
+
+        results.forEach((layer) => {
+          loadedElements.push(loadLayerImg(layer));
+        });
+
+        await Promise.all(loadedElements).then((renderObjectArray) => {
+          debugLogs ? console.log("Clearing canvas") : null;
+          ctx.clearRect(0, 0, format.width, format.height);
+          if (gif.export) {
+            hashlipsGiffer = new HashlipsGiffer(
+              canvas,
+              ctx,
+              `${buildDir}/gifs/${abstractedIndexes[0]}.gif`,
+              gif.repeat,
+              gif.quality,
+              gif.delay
+            );
+            hashlipsGiffer.start();
+          }
+          if (background.generate) {
+            drawBackground();
+          }
+          elepantBodyColor = getRandomBodyColor();
+          elepantPantsColor = getRandomPantsColor();
+          renderObjectArray.forEach((renderObject, index) => {
+            drawElement(
+              renderObject,
+              index,
+              layerConfigurations[layerConfigIndex].layersOrder.length
+            );
+            if (gif.export) {
+              hashlipsGiffer.add();
+            }
+          });
+          if (gif.export) {
+            hashlipsGiffer.stop();
+          }
+          debugLogs
+            ? console.log("Editions left to create: ", abstractedIndexes)
+            : null;
+          saveImage(abstractedIndexes[0]);
+          addMetadata(newDna, abstractedIndexes[0]);
+          saveMetaDataSingleFile(abstractedIndexes[0]);
+          console.log(
+            `Created edition: ${abstractedIndexes[0]}, with DNA: ${sha1(
+              newDna
+            )}`
+          );
+        });
+        dnaList.add(filterDNAOptions(newDna));
+        editionCount++;
+        abstractedIndexes.shift();
+      } else {
+        console.log("DNA exists!");
+        failedCount++;
+        if (failedCount >= uniqueDnaTorrance) {
+          console.log(
+            `You need more layers or elements to grow your edition to ${layerConfigurations[layerConfigIndex].growEditionSizeTo} artworks!`
+          );
+          process.exit();
+        }
+      }
+    }
+    layerConfigIndex++;
+  }
+  writeMetaData(JSON.stringify(metadataList, null, 2));
+};
+
+module.exports = { startCreating, buildSetup, getElements };
